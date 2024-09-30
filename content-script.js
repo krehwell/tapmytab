b655f27afdca3548c2d7a3b92cdc95f38b148ed6
krehwell <fahrimuham001@gmail.com>
2024-10-01 05:54:39 +0700

    iframe to work

--- content-script.js
+++
@@ -1,7 +0,0 @@
-const src = chrome.runtime.getURL('pages/iframe.html')
-
-const iframe = new DOMParser().parseFromString(
-    `<iframe class="crx" src="${src}"></iframe>`
-).body.firstElementChild
-
-document.body.append(iframe)
--- src/content.tsx
+++ src/content.tsx
@@ -3,9 +3,32 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './components/App'
 
-const root = document.createElement('div')
-root.id = 'crx-root'
-document.body.appendChild(root)
+const buildRoot = () => {
+    const root = document.createElement('div')
+    root.id = 'crx-root'
+    document.body.appendChild(root)
+    return root
+}
+
+const dumpIFrame = () => {
+    const iframe = document.createElement('iframe')
+    iframe.src = chrome.runtime.getURL('pages/iframe.html')
+    iframe.style.cssText = `
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    width: 300px;
+    height: 250px;
+    border: none;
+    z-index: 9999;
+`
+    document.body.appendChild(iframe)
+}
+
+// =========================================================================
+
+const root = buildRoot()
+// dumpIFrame()
 
 // this is for extension main page
 ReactDOM.createRoot(root).render(
