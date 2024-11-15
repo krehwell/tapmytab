84b61121bb15e6bf7a3c483499ee1e2e32e9cf3f
krehwell <fahrimuham001@gmail.com>
2024-11-15 07:28:49 +0700

    only show option when its a board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -140,7 +140,7 @@ export const Board = ({
                         }
                     }}
                 />
-                <BoardOptions />
+                {!isPlaceholder && <BoardOptions />}
             </FlexRowAlignCenter>
 
             {/* CARD LIST */}
