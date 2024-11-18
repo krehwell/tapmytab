df533db07bbca0a1bc17eec148c5d0bfd693c25d
krehwell <fahrimuham001@gmail.com>
2024-11-18 09:26:54 +0700

    allow build with error

--- src/main.tsx
+++ src/main.tsx
@@ -10,29 +10,10 @@ const buildRoot = () => {
     return root
 }
 
-const dumpIFrame = () => {
-    const iframe = document.createElement('iframe')
-    iframe.src = chrome.runtime.getURL('pages/iframe.html')
-    iframe.style.cssText = `
-    position: fixed;
-    top: 20px;
-    right: 20px;
-    width: 300px;
-    height: 250px;
-    border: none;
-    z-index: 9999;
-`
-    document.body.appendChild(iframe)
-}
-
-// =========================================================================
-
 const root = buildRoot()
-// dumpIFrame()
 
-// this is for extension main page
 ReactDOM.createRoot(root).render(
     <React.StrictMode>
-        <App isExtension />
+        <App />
     </React.StrictMode>
 )
