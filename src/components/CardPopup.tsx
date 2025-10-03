11a09a2fb9dc2f4321f1187efb7dce7207974f23
krehwell <fahrimuham001@gmail.com>
2025-10-04 03:43:13 +0700

    due date

--- src/components/CardPopup.tsx
+++ src/components/CardPopup.tsx
@@ -1,228 +1,248 @@
-import { create } from 'zustand'
-import Dialog from '@mui/material/Dialog'
-import { FlexColumn, FlexRowAlignCenter } from './Flex/index.tsx'
-import { tc } from '../utils/themeColors.ts'
-import Box from '@mui/material/Box'
-import { TCard } from '../types.ts'
-import { Label } from './Label.tsx'
-import { Editor, useEditorInstance } from './Editor/Editor.tsx'
-import { Button } from './Button.tsx'
-import { Toolbar } from './Editor/Toolbar/Toolbar.tsx'
-import { Editor as TiptapEditor } from '@tiptap/react'
-import { DotsThree, X } from '@phosphor-icons/react'
-import { WithOptionsMenu } from './WithOptionsMenu.tsx'
-import TextareaAutosize from '@mui/material/TextareaAutosize'
-import { LABELS } from '../utils/label.ts'
-import { deleteCard, updateCard } from '../stores/useCardStore.ts'
+import { useRef, useState } from "react";
+import { create } from "zustand";
+import Dialog from "@mui/material/Dialog";
+import { FlexColumn, FlexRowAlignCenter } from "./Flex/index.tsx";
+import { tc } from "../utils/themeColors.ts";
+import Box from "@mui/material/Box";
+import { TCard } from "../types.ts";
+import { Label } from "./Label.tsx";
+import { Editor, useEditorInstance } from "./Editor/Editor.tsx";
+import { Button } from "./Button.tsx";
+import { Toolbar } from "./Editor/Toolbar/Toolbar.tsx";
+import { Editor as TiptapEditor } from "@tiptap/react";
+import { CalendarDots, DotsThree, X } from "@phosphor-icons/react";
+import { WithOptionsMenu } from "./WithOptionsMenu.tsx";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import { LABELS } from "../utils/label.ts";
+import { deleteCard, updateCard } from "../stores/useCardStore.ts";
+import dayjs from "dayjs";
+import { Due } from "./Due.tsx";
 
 export const useCardPopupStore = create<{
-    isOpen: boolean
-    closePopup: () => void
-    openPopup: (props: { card: TCard; sortableCheat: string }) => void
-    sortableCheat: string | null
-    card: TCard | null
-    updateField: (props: { fields: Partial<TCard> }) => void
-    editor: TiptapEditor | null
+	isOpen: boolean;
+	closePopup: () => void;
+	openPopup: (props: { card: TCard; sortableCheat: string }) => void;
+	sortableCheat: string | null;
+	card: TCard | null;
+	updateField: (props: { fields: Partial<TCard> }) => void;
+	editor: TiptapEditor | null;
 }>((set, get) => ({
-    isOpen: false,
-    sortableCheat: null,
-    card: null,
-    editor: null,
-    closePopup: () => set({ isOpen: false, card: null }),
-    updateField: ({ fields }) => {
-        const card = Object.assign({}, get().card)
-        for (const key in fields) {
-            card[key] = fields[key]
-        }
-        set({ card })
-    },
-    openPopup: ({ card, sortableCheat }) => set({ isOpen: true, card, sortableCheat }),
-}))
+	isOpen: false,
+	sortableCheat: null,
+	card: null,
+	editor: null,
+	closePopup: () => set({ isOpen: false, card: null }),
+	updateField: ({ fields }) => {
+		const card = Object.assign({}, get().card);
+		for (const key in fields) {
+			card[key] = fields[key];
+		}
+		set({ card });
+	},
+	openPopup: ({ card, sortableCheat }) =>
+		set({ isOpen: true, card, sortableCheat }),
+}));
 
 export const CardPopup = () => {
-    const isOpen = useCardPopupStore((s) => s.isOpen)
+	const isOpen = useCardPopupStore((s) => s.isOpen);
 
-    return (
-        <Dialog
-            open={isOpen}
-            transitionDuration={0}
-            PaperComponent={({ children, style, ...props }) => (
-                <Box
-                    component={FlexColumn}
-                    style={{
-                        backgroundColor: tc.bgPrimary,
-                        borderRadius: '1.2rem',
-                        padding: '1.6rem',
-                        width: '62.4rem',
-                        ...style,
-                    }}
-                    {...props}
-                >
-                    {children}
-                </Box>
-            )}
-        >
-            <CardPopupHeader />
-            <CardPopupEditor key={String(isOpen)} />
-            <div style={{ marginBottom: '2rem' }} />
-            <CardPopupActions />
-        </Dialog>
-    )
-}
+	return (
+		<Dialog
+			open={isOpen}
+			transitionDuration={0}
+			PaperComponent={({ children, style, ...props }) => (
+				<Box
+					component={FlexColumn}
+					style={{
+						backgroundColor: tc.bgPrimary,
+						borderRadius: "1.2rem",
+						padding: "1.6rem",
+						width: "62.4rem",
+						...style,
+					}}
+					{...props}
+				>
+					{children}
+				</Box>
+			)}
+		>
+			<CardPopupHeader />
+			<CardPopupEditor key={String(isOpen)} />
+			<div style={{ marginBottom: "2rem" }} />
+			<CardPopupActions />
+		</Dialog>
+	);
+};
 
 const CardPopupHeader = () => {
-    const card = useCardPopupStore((s) => s.card)
-    const sortableCheat = useCardPopupStore((s) => s.sortableCheat)
-    const closePopup = useCardPopupStore((s) => s.closePopup)
-    const updateField = useCardPopupStore((s) => s.updateField)
+	const card = useCardPopupStore((s) => s.card);
+	const sortableCheat = useCardPopupStore((s) => s.sortableCheat);
+	const closePopup = useCardPopupStore((s) => s.closePopup);
+	const updateField = useCardPopupStore((s) => s.updateField);
 
-    if (!card) return null
+	if (!card) return null;
 
-    return (
-        <>
-            <FlexRowAlignCenter style={{ marginBottom: '0.8rem' }}>
-                <TextareaAutosize
-                    maxRows={1}
-                    maxLength={60}
-                    defaultValue={card?.title}
-                    placeholder='Add Title...'
-                    onChange={(e) => updateField({ fields: { title: e.target.value } })}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') e.currentTarget.blur()
-                    }}
-                    style={{
-                        backgroundColor: 'transparent',
-                        flex: 1,
-                        resize: 'none',
-                        fontSize: '3.1rem',
-                        fontWeight: 'bold',
-                    }}
-                />
-                <WithOptionsMenu
-                    options={[
-                        {
-                            label: 'Delete Card',
-                            onClick: () => {
-                                deleteCard({ sortableCheat: sortableCheat! })
-                                closePopup()
-                            },
-                        },
-                    ]}
-                >
-                    {({ openMenu }) => (
-                        <Button radius='3.2rem' onClick={openMenu} tabIndex={-1}>
-                            <DotsThree size={22} weight='bold' style={{ flexShrink: 0 }} />
-                        </Button>
-                    )}
-                </WithOptionsMenu>
-                <Button radius='3.2rem' onClick={closePopup} tabIndex={-1}>
-                    <X size={22} />
-                </Button>
-            </FlexRowAlignCenter>
-            <TextareaAutosize
-                maxRows={2}
-                maxLength={128}
-                defaultValue={card?.desc}
-                placeholder='Add description...'
-                onChange={(e) => updateField({ fields: { desc: e.target.value } })}
-                style={{
-                    backgroundColor: 'transparent',
-                    resize: 'none',
-                    fontSize: '1.6rem',
-                    color: 'rgba(248, 249, 250, 0.80)',
-                }}
-            />
-            <WithOptionsMenu
-                menuProps={{
-                    sx: {
-                        '.MuiList-root': {
-                            display: 'flex',
-                            flexDirection: 'row',
-                        },
-                    },
-                    anchorOrigin: { vertical: 'center', horizontal: 'right' },
-                    transformOrigin: { vertical: 'center', horizontal: 'left' },
-                }}
-                options={[...LABELS].map((l) => ({
-                    label: l,
-                    node: <Label label={l} style={{ cursor: 'pointer' }} />,
-                    onClick: () => updateField({ fields: { label: l } }),
-                }))}
-            >
-                {({ openMenu }) => {
-                    return (
-                        <Label
-                            label={card.label}
-                            style={{ marginTop: '0.8rem' }}
-                            onClick={(e) => {
-                                e.stopPropagation()
-                                openMenu(e)
-                            }}
-                        />
-                    )
-                }}
-            </WithOptionsMenu>
-            <div style={{ marginBottom: '2rem' }} />
-        </>
-    )
-}
+	return (
+		<>
+			<FlexRowAlignCenter style={{ marginBottom: "0.8rem" }}>
+				<TextareaAutosize
+					maxRows={1}
+					maxLength={60}
+					defaultValue={card?.title}
+					placeholder="Add Title..."
+					onChange={(e) => updateField({ fields: { title: e.target.value } })}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") e.currentTarget.blur();
+					}}
+					style={{
+						backgroundColor: "transparent",
+						flex: 1,
+						resize: "none",
+						fontSize: "3.1rem",
+						fontWeight: "bold",
+					}}
+				/>
+				<WithOptionsMenu
+					options={[
+						{
+							label: "Delete Card",
+							onClick: () => {
+								deleteCard({ sortableCheat: sortableCheat! });
+								closePopup();
+							},
+						},
+					]}
+				>
+					{({ openMenu }) => (
+						<Button radius="3.2rem" onClick={openMenu} tabIndex={-1}>
+							<DotsThree size={22} weight="bold" style={{ flexShrink: 0 }} />
+						</Button>
+					)}
+				</WithOptionsMenu>
+				<Button radius="3.2rem" onClick={closePopup} tabIndex={-1}>
+					<X size={22} />
+				</Button>
+			</FlexRowAlignCenter>
+			<TextareaAutosize
+				maxRows={2}
+				maxLength={128}
+				defaultValue={card?.desc}
+				placeholder="Add description..."
+				onChange={(e) => updateField({ fields: { desc: e.target.value } })}
+				style={{
+					backgroundColor: "transparent",
+					resize: "none",
+					fontSize: "1.6rem",
+					color: "rgba(248, 249, 250, 0.80)",
+				}}
+			/>
+			<FlexRowAlignCenter>
+				<CardPopupOptionsLabel card={card} />
+				<Due
+					initialDueDate={card?.dueDate || null}
+					onChange={({ dueDate }) => {
+						updateField({ fields: { dueDate } });
+					}}
+				/>
+			</FlexRowAlignCenter>
+			<div style={{ marginBottom: "2rem" }} />
+		</>
+	);
+};
+
+const CardPopupOptionsLabel = ({ card }: { card: TCard }) => {
+	const updateField = useCardPopupStore((s) => s.updateField);
+
+	return (
+		<WithOptionsMenu
+			menuProps={{
+				sx: {
+					".MuiList-root": {
+						display: "flex",
+						flexDirection: "row",
+					},
+				},
+				anchorOrigin: { vertical: "center", horizontal: "right" },
+				transformOrigin: { vertical: "center", horizontal: "left" },
+			}}
+			options={[...LABELS].map((l) => ({
+				label: l,
+				node: <Label label={l} style={{ cursor: "pointer" }} />,
+				onClick: () => updateField({ fields: { label: l } }),
+			}))}
+		>
+			{({ openMenu }) => {
+				return (
+					<Label
+						label={card.label}
+						style={{ marginTop: "0.8rem" }}
+						onClick={(e) => {
+							e.stopPropagation();
+							openMenu(e);
+						}}
+					/>
+				);
+			}}
+		</WithOptionsMenu>
+	);
+};
 
 const CardPopupEditor = () => {
-    const card = useCardPopupStore((s) => s.card)
-    const updateField = useCardPopupStore((s) => s.updateField)
+	const card = useCardPopupStore((s) => s.card);
+	const updateField = useCardPopupStore((s) => s.updateField);
 
-    const { editor } = useEditorInstance({
-        content: card?.content || '',
-        onChange: ({ content }) => updateField({ fields: { content } }),
-        shouldRerenderOnTransaction: true,
-    })
+	const { editor } = useEditorInstance({
+		content: card?.content || "",
+		onChange: ({ content }) => updateField({ fields: { content } }),
+		shouldRerenderOnTransaction: true,
+	});
 
-    if (!editor) return null
+	if (!editor) return null;
 
-    return (
-        <>
-            <Editor
-                editor={editor}
-                style={{
-                    overflow: 'hidden auto',
-                    backgroundColor: '#2C3034',
-                    borderRadius: '0.8rem 0.8rem 0rem 0rem',
-                    minHeight: '21.2rem',
-                    maxHeight: '55rem',
-                }}
-            />
-            <Toolbar editor={editor} />
-        </>
-    )
-}
+	return (
+		<>
+			<Editor
+				editor={editor}
+				style={{
+					overflow: "hidden auto",
+					backgroundColor: "#2C3034",
+					borderRadius: "0.8rem 0.8rem 0rem 0rem",
+					minHeight: "21.2rem",
+					maxHeight: "55rem",
+				}}
+			/>
+			<Toolbar editor={editor} />
+		</>
+	);
+};
 
 const CardPopupActions = () => {
-    const sortableCheat = useCardPopupStore((s) => s.sortableCheat)
-    const card = useCardPopupStore((s) => s.card)
-    const closePopup = useCardPopupStore((s) => s.closePopup)
+	const sortableCheat = useCardPopupStore((s) => s.sortableCheat);
+	const card = useCardPopupStore((s) => s.card);
+	const closePopup = useCardPopupStore((s) => s.closePopup);
 
-    return (
-        <FlexRowAlignCenter style={{ gap: '1.2rem', marginLeft: 'auto' }}>
-            <Button
-                onClick={closePopup}
-                style={{ width: '6.8rem', height: '3.3rem', borderRadius: '0.8rem' }}
-            >
-                Cancel
-            </Button>
-            <Button
-                onClick={() => {
-                    updateCard({ sortableCheat: sortableCheat!, fields: card! })
-                    closePopup()
-                }}
-                sx={{
-                    backgroundColor: '#4C5257',
-                    width: '6.8rem',
-                    height: '3.3rem',
-                    borderRadius: '0.8rem',
-                }}
-            >
-                Save
-            </Button>
-        </FlexRowAlignCenter>
-    )
-}
+	return (
+		<FlexRowAlignCenter style={{ gap: "1.2rem", marginLeft: "auto" }}>
+			<Button
+				onClick={closePopup}
+				style={{ width: "6.8rem", height: "3.3rem", borderRadius: "0.8rem" }}
+			>
+				Cancel
+			</Button>
+			<Button
+				onClick={() => {
+					updateCard({ sortableCheat: sortableCheat!, fields: card! });
+					closePopup();
+				}}
+				sx={{
+					backgroundColor: "#4C5257",
+					width: "6.8rem",
+					height: "3.3rem",
+					borderRadius: "0.8rem",
+				}}
+			>
+				Save
+			</Button>
+		</FlexRowAlignCenter>
+	);
+};
--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,22 @@
 export type TCard = {
-    id: string
-    title: string
-    desc?: string
-    content: string
-    label: TLabel
-}
+	id: string;
+	title: string;
+	desc?: string;
+	content: string;
+	label: TLabel;
+	dueDate?: null | string;
+};
 
 export type TBoard = {
-    id: string
-    cards: TCard[]
-    name?: string
-}
+	id: string;
+	cards: TCard[];
+	name?: string;
+};
 
 export enum TLabel {
-    Red = 'red',
-    Green = 'green',
-    Blue = 'blue',
-    Yellow = 'yellow',
-    No = 'transparent'
+	Red = "red",
+	Green = "green",
+	Blue = "blue",
+	Yellow = "yellow",
+	No = "transparent",
 }
