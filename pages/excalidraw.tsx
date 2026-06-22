732deb56cfdf7f721d6902140476738c26f12afe
krehwell <fahrimuham001@gmail.com>
2026-06-22 20:09:27 +0700

    chore: excalidraw autofocus

--- pages/excalidraw.tsx
+++ pages/excalidraw.tsx
@@ -61,6 +61,7 @@ const App = () => {
     return (
         <Excalidraw
             theme='dark'
+            autoFocus
             excalidrawAPI={(api) => {
                 setTimeout(() => api.scrollToContent(undefined, { fitToContent: true }), 10)
             }}
